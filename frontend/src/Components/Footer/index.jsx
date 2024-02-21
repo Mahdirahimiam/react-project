ee71439581275565fddae62e479c626e5bfd6f61
Mahdirahimiam <146871239+Mahdirahimiam@users.noreply.github.com>
2024-02-21 15:24:44 +0330

    creat footer
    design footer

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -1,7 +1,33 @@
+import { Box, List, ListItem, ListItemSecondaryAction, ListItemText } from '@mui/material'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <List>
+        <ListItem>
+          <ListItemText>
+            خدمات مشتریان
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+          پیگیری سفارش
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+          قوانین و مقررات
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemSecondaryAction>
+            <ListItemText>
+            ثبت شکایات در سایت
+            </ListItemText>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </>
   )
 }
