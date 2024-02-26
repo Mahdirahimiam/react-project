3dd5c59e33e134706cfd3da3131f4d6a3b0d33ea
Mahdirahimiam <146871239+Mahdirahimiam@users.noreply.github.com>
2024-02-26 15:00:02 +0330

    fonts
    import fonts into project.

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -1,33 +1,32 @@
-import { Box, List, ListItem, ListItemSecondaryAction, ListItemText } from '@mui/material'
+import { Box, List, ListItem, ListItemSecondaryAction, ListItemText, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Footer() {
   return (
     <>
-      <List>
-        <ListItem>
-          <ListItemText>
-            خدمات مشتریان
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-          پیگیری سفارش
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-          قوانین و مقررات
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemSecondaryAction>
-            <ListItemText>
-            ثبت شکایات در سایت
-            </ListItemText>
-          </ListItemSecondaryAction>
-        </ListItem>
-      </List>
+      <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+        <Stack direction={'row'} sx={{ flexWrap: 'wrap' }}>
+          <Box>
+            <Typography sx={{fontWeight:'bold'}} variant='body1' component={'p'}>
+              خدمات مشتریان
+            </Typography>
+            <Typography>پیگیری سفارش</Typography>
+            <Typography>قوانین و مقررات</Typography>
+            <Typography>ثبت شکایات در سایت</Typography>
+          </Box>
+
+          <Box>
+            <Typography sx={{fontWeight:'bold'}} variant='body1' component={'p'}>
+فروشگاه
+            </Typography>
+            <Typography>درباره ما</Typography>
+            <Typography>تماس با ما</Typography>
+            <Typography>نقشه سایت</Typography>
+          </Box>
+        </Stack>
+        <Box>
+        </Box>
+      </Stack>
     </>
   )
 }
